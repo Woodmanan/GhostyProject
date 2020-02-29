bf8a9c127293800e444343d39f73d46e18d136e2
caseyot6 <55035980+caseyot6@users.noreply.github.com>
2020-02-28 23:49:10 -0500

    Area signal

---
+++ GiveUpTheGhost/Assets/AreaSignal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaSignal : MonoBehaviour
+{
+    // Objects react to this object, this object does not react, it only stores information
+    public int state_0 = 0;
+    public int state_1 = 0;
+    public int state_2 = 0;
+    public int state_3 = 0;
+    public bool jump = false;
+    public bool reverse = false;
+    public bool teleport = false;
+    public AreaSignal teleport_to;
+
+    void Awake()
+    {
+        SpriteRenderer symbol = transform.Find("Symbol").GetComponent<SpriteRenderer>();
+        //symbol.color = new Color32(0, 0, 0, 0);
+    }
+    
+    void Start()
+    {
+
+
+    }
+
+}
--- GiveUpTheGhost/Assets/Monster.cs
+++ GiveUpTheGhost/Assets/Monster.cs
@@ -5,14 +5,143 @@ using UnityEngine;
 public class Monster : MonoBehaviour
 {
     // Start is called before the first frame update
+    private List<string> floors = new List<string>() { "Floor", "Platform" };
+
+
+    private float left_limit;
+    private float right_limit;
+    private int curr_direction = 0;
+
+    public float boundary;
+    public float jumpForce;
+    public bool inAir;
+    
+    public float speed;
+    private int accel;
+
+    public float horizontal_margin;
+
+    public int enemyType = 0;
+    private Rigidbody2D thisBody;
+
+    private bool isJumping = false;
+    
     void Start()
     {
-        
+
+    }
+    void Awake()
+    {
+        thisBody = GetComponent<Rigidbody2D>();
+        thisBody.freezeRotation = true;
+
+        left_limit = transform.localPosition.x - boundary;
+        right_limit = transform.localPosition.x + boundary;
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+        if (transform.localPosition.x < left_limit + horizontal_margin)
+        {
+            if (curr_direction == 1)
+            {
+                curr_direction = 0;
+            }
+        }
+
+        if (transform.localPosition.x > right_limit - horizontal_margin)
+        {
+            if (curr_direction == 0)
+            {
+                curr_direction = 1;
+            }
+        }
+
+        if (enemyType == 0)
+        {
+            enemyTypeZero();
+        }
+
+    }
+
+
+    void enemyTypeZero()
+    {
+
+        Vector3 newPosition = transform.localPosition;
+
+        if (curr_direction == 0)
+        {
+
+            newPosition = speed * Vector3.right;
+
+        }
+        else if (curr_direction == 1)
+        {
+
+            newPosition = speed * Vector3.left;
+
+        }
+
+        if (isJumping)
+        {
+            Debug.Log("jumpforce");
+            thisBody.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+            isJumping = false;
+            inAir = true;
+
+        }
+
+        transform.Translate(newPosition);
+        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.name == "AreaSignal")
+        {
+
+            AreaSignal signalSpace = collider.gameObject.GetComponent<AreaSignal>();
+
+            if (((signalSpace.jump == true) && (isJumping == false)) && inAir == false)
+            {
+                isJumping = true;
+            }
+
+            if (signalSpace.reverse == true)
+            {
+            }
+
+
+        }
+    }
+
+    
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
         
+        if (inAir)
+        {
+
+            if (floors.Contains(collision.gameObject.name))
+            {
+                inAir = false;
+            }
+
+
+        }
+
+
+
+
+
+
+
     }
+
+
 }
