6ae9f5dded0d9fac2699407b6eb946554e74534b
Woodmanan <mwmccoy@andrew.cmu.edu>
2020-03-20 15:53:47 -0400

    Bossfight demo intro done, added new environ stuff
    Finished up the demo intro and exit sequences for the bossfight, added in new damaging objects, a shield to block them, and moving platforms

---
+++ GiveUpTheGhost/Assets/FallingDamage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingDamage : MonoBehaviour
+{
+    [SerializeField] private int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Body"))
+        {
+            other.gameObject.GetComponent<Character>().TakeDamage(damage);
+        }
+        //TODO: Fill this in for monsters
+        print("Collided with something!");
+        StartCoroutine(DestroyThis(2f));
+    }
+
+    IEnumerator DestroyThis(float delay)
+    {
+        GetComponent<Rigidbody2D>().simulated = false;
+        GetComponent<CircleCollider2D>().enabled = false;
+        GetComponent<ParticleSystem>().Play();
+        GetComponent<SpriteRenderer>().enabled = false;
+        yield return new WaitForSeconds(delay);
+        Destroy(this.gameObject);
+    }
+}
---
+++ GiveUpTheGhost/Assets/Shield.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    private Possessable poss;
+
+    private GameObject body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        poss = GetComponent<Possessable>();
+        body = GameObject.FindGameObjectWithTag("Body");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (poss.possessed)
+        {
+            Orient();
+        }
+    }
+
+    private void Orient()
+    {
+        Vector3 offset = transform.position - body.transform.position;
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0,0, angle + 90);
+    }
+}
