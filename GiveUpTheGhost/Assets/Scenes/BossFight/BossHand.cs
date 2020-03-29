36f10a21bf49fbe2a228718f5accd26498288526
caseyot6 <55035980+caseyot6@users.noreply.github.com>
2020-03-29 01:49:04 -0400

    Revert "fix"
    This reverts commit 165bc6d4142e8417bca4a291eff88a530baf0ae3.

--- GiveUpTheGhost/Assets/Scenes/BossFight/BossHand.cs
+++ GiveUpTheGhost/Assets/Scenes/BossFight/BossHand.cs
@@ -7,10 +7,11 @@ public class BossHand : MonoBehaviour
     // Start is called before the first frame update
     private bool possessed = false;
     Rigidbody2D currentBody;
-    Vector2 currVelocity;
 
     private double speed = 300;
     double currentDirectionDegrees;
+    private double leftBoundary;
+    private double rightBoundary;
     private Dictionary<int, Vector2> angles;
 
     private double possessionDelay = 2;
@@ -24,16 +25,17 @@ public class BossHand : MonoBehaviour
     void Start()
     {
         currentBody = transform.gameObject.GetComponent<Rigidbody2D>();
-       
+        leftBoundary = transform.localPosition.x + transform.parent.gameObject.GetComponent<Boss>().armsRange * -1;
+        rightBoundary = transform.localPosition.y + transform.parent.gameObject.GetComponent<Boss>().armsRange;
 
-        Vector2 zero = new Vector2(Mathf.Cos(30*Mathf.Deg2Rad), Mathf.Sin(30*Mathf.Deg2Rad));
-        Vector2 one = new Vector2(Mathf.Cos(45 * Mathf.Deg2Rad), Mathf.Sin(45 * Mathf.Deg2Rad));
-        Vector2 two = new Vector2(Mathf.Cos(60 * Mathf.Deg2Rad), Mathf.Sin(60 * Mathf.Deg2Rad));
-        Vector2 three = new Vector2(Mathf.Cos(120 * Mathf.Deg2Rad), Mathf.Sin(120 * Mathf.Deg2Rad));
-        Vector2 four = new Vector2(Mathf.Cos(135 * Mathf.Deg2Rad), Mathf.Sin(135 * Mathf.Deg2Rad));
-        Vector2 five = new Vector2(Mathf.Cos(150 * Mathf.Deg2Rad), Mathf.Sin(150 * Mathf.Deg2Rad));
+        Vector2 zero = new Vector2(Mathf.Sin(30*Mathf.Deg2Rad), Mathf.Sin(30*Mathf.Deg2Rad));
+        Vector2 one = new Vector2(Mathf.Sin(45 * Mathf.Deg2Rad), Mathf.Sin(45 * Mathf.Deg2Rad));
+        Vector2 two = new Vector2(Mathf.Sin(60 * Mathf.Deg2Rad), Mathf.Sin(60 * Mathf.Deg2Rad));
+        Vector2 three = new Vector2(Mathf.Sin(120 * Mathf.Deg2Rad), Mathf.Sin(120 * Mathf.Deg2Rad));
+        Vector2 four = new Vector2(Mathf.Sin(135 * Mathf.Deg2Rad), Mathf.Sin(135 * Mathf.Deg2Rad));
+        Vector2 five = new Vector2(Mathf.Sin(150 * Mathf.Deg2Rad), Mathf.Sin(150 * Mathf.Deg2Rad));
 
-        currVelocity = new Vector2(0, -1);
+        currentBody.velocity = new Vector2(0, -1);
         
         angles = new Dictionary<int, Vector2>{
             {0,  zero},
@@ -53,7 +55,7 @@ public class BossHand : MonoBehaviour
             {
                 possessed = false;
                 possessionTimer = .01;
-                currVelocity = new Vector2(0,1);
+                currentBody.velocity = new Vector2(0,-1);
                 
             }
 
@@ -66,8 +68,8 @@ public class BossHand : MonoBehaviour
                     if (possessionTimer == 0)
                     {
 
-                        lastVelocity = currVelocity;
-                        currVelocity = new Vector2(0, 0);
+                        lastVelocity = currentBody.velocity;
+                        currentBody.velocity = new Vector2(0, 0);
                         possessed = true;
 
 
@@ -86,7 +88,7 @@ public class BossHand : MonoBehaviour
             {
                 possessed = false;
                 possessionTimer = .01;
-                currVelocity = new Vector2(0,1);
+                currentBody.velocity = new Vector2(0,-1);
 
             }
         }
@@ -102,7 +104,7 @@ public class BossHand : MonoBehaviour
         else
         {
             
-            currentBody.velocity = currVelocity.normalized * (float)speed * Time.deltaTime;
+            currentBody.velocity = currentBody.velocity.normalized * (float)speed * Time.deltaTime;
         }
 
         if (possessionTimer >= .01)
@@ -123,15 +125,14 @@ public class BossHand : MonoBehaviour
         {
             if (collision.collider.name != "Character")
             {
-                
-                double randomNum = (double)Random.value - 1;
-                if (randomNum < 0)
+                if (Random.Range(0, 3) > 1.5)
                 {
-                    currVelocity = angles[Random.Range(0, 5)];
+                    currentBody.velocity = angles[Random.Range(0, 5)] * -1;
+
                 }
-                else
+                if (Random.Range(0, 3) < 1.5)
                 {
-                    currVelocity = angles[Random.Range(0, 5)]*-1;
+                    currentBody.velocity = angles[Random.Range(0, 5)];
 
                 }
 
@@ -148,7 +149,7 @@ public class BossHand : MonoBehaviour
             if (collision.collider.transform.position.y - currentBody.transform.position.y > 0)
             {
                 Debug.Log("CharacterIsHere");
-                currVelocity = new Vector2(0, 0);
+                currentBody.velocity = new Vector2(0, 0);
 
             }
         }
@@ -160,7 +161,7 @@ public class BossHand : MonoBehaviour
         if (collision.collider.name == "Character")
         {
 
-            currVelocity = new Vector2(0, 1);
+            currentBody.velocity = new Vector2(0, 1);
         }
     }
 
