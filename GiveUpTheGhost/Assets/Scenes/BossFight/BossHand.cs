3b18419496a545b4de72eb87b8b2c17491944051
caseyot6 <55035980+caseyot6@users.noreply.github.com>
2020-03-29 02:19:45 -0400

    tweak
    tweak

--- GiveUpTheGhost/Assets/Scenes/BossFight/BossHand.cs
+++ GiveUpTheGhost/Assets/Scenes/BossFight/BossHand.cs
@@ -9,9 +9,6 @@ public class BossHand : MonoBehaviour
     Rigidbody2D currentBody;
 
     private double speed = 300;
-    double currentDirectionDegrees;
-    private double leftBoundary;
-    private double rightBoundary;
     private Dictionary<int, Vector2> angles;
 
     private double possessionDelay = 2;
@@ -25,9 +22,7 @@ public class BossHand : MonoBehaviour
     void Start()
     {
         currentBody = transform.gameObject.GetComponent<Rigidbody2D>();
-        leftBoundary = transform.localPosition.x + transform.parent.gameObject.GetComponent<Boss>().armsRange * -1;
-        rightBoundary = transform.localPosition.y + transform.parent.gameObject.GetComponent<Boss>().armsRange;
-
+     
         Vector2 zero = new Vector2(Mathf.Sin(30*Mathf.Deg2Rad), Mathf.Sin(30*Mathf.Deg2Rad));
         Vector2 one = new Vector2(Mathf.Sin(45 * Mathf.Deg2Rad), Mathf.Sin(45 * Mathf.Deg2Rad));
         Vector2 two = new Vector2(Mathf.Sin(60 * Mathf.Deg2Rad), Mathf.Sin(60 * Mathf.Deg2Rad));
@@ -35,7 +30,7 @@ public class BossHand : MonoBehaviour
         Vector2 four = new Vector2(Mathf.Sin(135 * Mathf.Deg2Rad), Mathf.Sin(135 * Mathf.Deg2Rad));
         Vector2 five = new Vector2(Mathf.Sin(150 * Mathf.Deg2Rad), Mathf.Sin(150 * Mathf.Deg2Rad));
 
-        currentBody.velocity = new Vector2(0, -1);
+        currentBody.velocity = new Vector2(Random.Range(-1,1), -1);
         
         angles = new Dictionary<int, Vector2>{
             {0,  zero},
@@ -125,12 +120,13 @@ public class BossHand : MonoBehaviour
         {
             if (collision.collider.name != "Character")
             {
-                if (Random.Range(0, 3) > 1.5)
+                
+                if (Random.Range(0, 4) > 1.5)
                 {
                     currentBody.velocity = angles[Random.Range(0, 5)] * -1;
 
                 }
-                if (Random.Range(0, 3) < 1.5)
+                else
                 {
                     currentBody.velocity = angles[Random.Range(0, 5)];
 
